74163b8012a4ea602c92583c9ab638cb7de98682
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2023-05-17 13:06:03 +0530

    added co-routine and multi instantiate
    and improved script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,40 +1,52 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject Ball;
-    [SerializeField]
-    private int Force = 50;
-    private bool IsAiming = false;
-    void Update()
+    [SerializeField] private GameObject ball;
+    [SerializeField] private int force = 50;
+    [SerializeField][Range(0, 100)] private int ballValue = 20;
+    private bool shooting = false;
+    private bool isAiming = false;
+    private void Update()
     {
-        //getting direction to shoot
-        Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //getting direction to Aim & shoot
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
 
-        AimingAt(MousePos);
-
-        ShootingAt(MousePos);
-    }
-
-    private void ShootingAt(Vector3 mousePos)
-    {
-        if (Input.GetMouseButtonUp(0) && IsAiming)
+        AimingAt(direction);
+        if(shooting == false)
         {
-            IsAiming = false;
-
-            //shooting ball towards mouse direction
-            GameObject ballInstance = Instantiate(Ball, transform.position, Quaternion.identity);
-            ballInstance.GetComponent<Rigidbody2D>().AddForce(mousePos.normalized * Force, ForceMode2D.Impulse);
+            StartCoroutine(ShootingAt(direction));
         }
     }
 
-    private void AimingAt(Vector3 mousePos)
+    private void AimingAt(Vector3 direction)
     {
         if (Input.GetMouseButton(0))
         {
-            IsAiming = true;
-            transform.LookAt(mousePos, Vector3.forward);
+            isAiming = true;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    private IEnumerator ShootingAt(Vector3 direction)
+    {
+        if (Input.GetMouseButtonUp(0) && isAiming)
+        {
+            isAiming = false;
+            shooting = true;
+            GameObject ballInstance;
+            for (int i=0; i < ballValue; i++)
+            {
+                ballInstance = Instantiate(ball, transform.position, Quaternion.identity);
+                ballInstance.GetComponent<Rigidbody2D>().AddForce(direction.normalized * force, ForceMode2D.Impulse);
+                yield return new WaitForSeconds(0.20f);
+            }
+            shooting = false;
         }
     }
 }
